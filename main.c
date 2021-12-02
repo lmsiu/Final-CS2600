017369d5c1482e385af676d367e559fa8bab2da7
lmsiu <lmsiu@cpp.edu>
2021-12-02 09:14:37 +0000

    Finished writing functions, added in built in functions, and included libs

--- main.c
+++ main.c
@@ -1,4 +1,25 @@
 //make a shell
+
+//libs and stuff we will need
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+//need to make some builtin shell commands
+//builtin shell command declaration
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+int lsh_num_bultins();
+
+//func declarations
+void lsh_loop();
+char *lsh_read_line();
+char **lsh_split_line(char *line);
+int lsh_excecute(char **args);
+
 int main(int argc, char **argv){
     //load any config files
 
@@ -99,7 +120,7 @@ char *lsh_read_line(){
 //parsing without using / or "" to seperate args
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
-char **lsh_split_line(){
+char **lsh_split_line(char *line){
     int bufsize = LSH_TOK_BUFSIZE, position = 0;
     char **tokens = malloc(bufsize * sizeof(char*));
     char *token;
@@ -110,7 +131,7 @@ char **lsh_split_line(){
     }
 
     //using the strtok lib to tokenize the string using whitespace delimeters
-    token = strtok(line, LSH_TOK_DELIM)
+    token = strtok(line, LSH_TOK_DELIM);
     while(token != NULL){
         tokens[position] = token;
         position++;
@@ -132,6 +153,92 @@ char **lsh_split_line(){
 
 }
 
+//function to excecute but dif than lsh_excecute
+int lsh_launch(char **args){
+    pid_t pid, wpid;
+    int status;
+
+    //makes a copy of the current process running (a child)
+    pid = fork();
+    //after fork there are now 2 processes running, parnet and child
+    if(pid == 0){
+        //child process
+        //uses execvp to replace the function
+        if(execvp(args[0], args)){
+            perror("lsh");
+        }
+        exit(EXIT_FAILURE);
+    //if fork() returns -1 there is an error
+    }else if(pid < 0){
+        //Error forking
+        perror("lsh Forked it up!");
+    } else{
+        //parent process
+        do{
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while(!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
+    //returns 1 to prompt more user input
+}
+
+//List of built in commands followed by their funcs
+char *builtin_str[] = { "cd", "help", "exit"};
+
+int (*builtin_func[]) (char **) = {
+    &lsh_cd, &lsh_help, &lsh_exit
+};
+
+//wrapper for lsh_launch and built ins
 int lsh_excecute(char **args){
-    
+    int i;
+
+    //if an empty command is entered
+    if(args[0] == NULL){
+        return 1;
+    }
+
+    for(i = 0; i < lsh_num_bultins(); i++){
+        if(strcmp(args[0], builtin_str[i]) == 0){
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    return lsh_launch(args);
+}
+
+//builtin func implimentations
+int lsh_cd(char **args){
+    if(args[1] == NULL){
+        fprintf(stderr, "lsh: Expected argument to \"cd\"\n");
+    }else{
+        if(chdir(args[1]) != 0){
+            perror("lsh");
+        }
+    }
+
+    return 1;
+}
+
+int lsh_num_bultins(){
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int lsh_help(char **args){
+    int i;
+    printf("Laura Siu's LSH\n");
+    printf("Type program names and arguments, and hit enter.\n");
+    printf("The following are built in:\n");
+
+    for( i = 0; i < lsh_num_bultins(); i++){
+        printf(" %s\n", builtin_str[i]);
+    }
+
+    printf("Please use the man command for information on other programs.\n");
+    return 1;
+}
+
+int lsh_exit(char **args){
+    return 0; 
 }
