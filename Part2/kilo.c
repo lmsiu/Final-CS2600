5e115387dcf4773f3beb5366dff9c0ddb429564f
lmsiu <lmsiu@cpp.edu>
2021-12-03 08:06:23 +0000

    Started steps to enter raw mode. Up to Step 11

--- Part2/kilo.c
+++ Part2/kilo.c
@@ -1,5 +1,52 @@
 //making a kilo text editor
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+
+struct termios orig_termios;
+
+void disableRawMode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode(){
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    //turn off the echo from canonical mode
+    struct termios raw = orig_termios;
+
+    //tcgetattr(STDIN_FILENO, &raw);
+
+    //disable ctrl-s and ctrl-q
+    raw.c_iflag &= ~(IXON);
+    //ISIG turns off Ctrl-C and Ctrl-Z
+    //ICANON turns off canonical mode so we can read input byte by byte instead of line by line
+    raw.c_lflag &= ~(ECHO | ICANON | ISIG);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(){
+    enableRawMode();
+
+    char c;
+    //read 1 character from the standaed input into c until there are no more bytes to read
+    //return 0 at the end of the file
+    //press q to quit when in canonical mode
+    //canonical mode needs to have enter pressed to read the line, we want raw mode
+    while(read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        //iscntrl tests if a char is a control char
+        if(iscntrl(c)){
+            printf("%d\n", c);
+        }else{
+            printf("%d ('%c')\n", c, c);
+        }
+        
+    }
+    
     return 0;
+    
 }
