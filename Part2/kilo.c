55b03e0a72db50f0f9110ade7bea08421a3863df
lmsiu <lmsiu@cpp.edu>
2021-12-08 22:23:31 +0000

    Added ability to scroll through search results

--- Part2/kilo.c
+++ Part2/kilo.c
@@ -535,17 +535,44 @@ void editorSave(){
 
 //incrimental searching
 void editorFindCallback(char *query, int key){
+    static int last_match = -1;
+    static int direction = 1;
+
+    //allows for scrolling through results
     if(key == '\r' || key == '\x1b'){
-        return;
+        last_match = -1;
+        direction = 1;
+        return;   
+    }else if(key == ARROW_RIGHT || key == ARROW_DOWN){
+        direction = 1;
+    }else if(key == ARROW_LEFT || key == ARROW_UP){
+        direction = -1;
+    }else{
+        last_match = -1;
+        direction = 1;
     }
 
+    if(last_match == -1){
+        direction =1;
+    }
+
+    int current = last_match;
     //actual searching
     int i;
     for(i = 0; i<E.numrows; i++){
-        erow *row = &E.row[i];
+        current += direction;
+
+        if(current == -1){
+            current = E.numrows -1;
+        }else if(current ==E.numrows){
+            current = 0;
+        }
+
+        erow *row = &E.row[current];
         char *match = strstr(row->render, query);
         if(match){
-            E.cy = i;
+            last_match = current;
+            E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
             break;
@@ -563,7 +590,7 @@ void editorFind(){
     int saved_rowoff = E.rowoff;
 
 
-    char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
+    char *query = editorPrompt("Search: %s (ESC to cancel, Arrows and Enter to move through results)", editorFindCallback);
     //if(query == NULL){
       //  return;
     //}
